b8c4ef4f6bd93aff3a8615dfba0d4be7dace9be2
Natalia Raffaele <natalia.rafe@gmail.com>
2018-05-17 17:35:09 +0100

    til form partial not finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+
+  get 'doses/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :cocktails, only: [:create, :new, :index, :show] do
+    resources :doses, only: [:create, :new]
+  end
+    resources :doses, only: :destroy
 end
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require 'open-uri'
+require 'json'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database'
+
+
+Ingredient.destroy_all if Rails.env.development?
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+ingredient = JSON.parse(open(url).read)
+ingredient["drinks"].each do |drink|
+  i = Ingredient.create(name: drink['strIngredient1'])
+  puts "Created #{i.name}"
+end
+
+
+puts 'Finished'
